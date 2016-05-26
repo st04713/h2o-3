3619981e4eccbdde458fd94df7a90053242f8bb2
wendycwong <wendycwong@yahoo.com>
2016-05-25 19:00:29 -0700

    PUBDEV-1843: Grid testing.  Subtask 2.  Reduce number of models allowed for glrm.

--- h2o-py/dynamic_tests/testdir_algos/glrm/pyunit_glrm_gridsearch_over_all_params_large.py
+++ h2o-py/dynamic_tests/testdir_algos/glrm/pyunit_glrm_gridsearch_over_all_params_large.py
@@ -45,7 +45,7 @@ class Test_glrm_grid_search:
     """
 
     # parameters set by users, change with care
-    max_grid_model = 150           # maximum number of grid models generated before adding max_runtime_secs
+    max_grid_model = 100           # maximum number of grid models generated before adding max_runtime_secs
 
     curr_time = str(round(time.time()))     # store current timestamp, used as part of filenames.
 
