bb6a34b14fd6918621a3e148870c4a8f49fecd3e
wendycwong <wendycwong@yahoo.com>
2016-05-26 10:47:45 -0700

    PUBDEV-1843: grid testing.  Subtask 2.  Reduce maximum model number of grid search models to cut down run time.

--- h2o-py/dynamic_tests/testdir_algos/kmeans/pyunit_kmeans_gridsearch_over_all_params_large.py
+++ h2o-py/dynamic_tests/testdir_algos/kmeans/pyunit_kmeans_gridsearch_over_all_params_large.py
@@ -39,7 +39,7 @@ class Test_kmeans_grid_search:
     """
 
     # parameters set by users, change with care
-    max_grid_model = 1000           # maximum number of grid models generated before adding max_runtime_secs
+    max_grid_model = 500           # maximum number of grid models generated before adding max_runtime_secs
 
     curr_time = str(round(time.time()))     # store current timestamp, used as part of filenames.
     seed = round(time.time())
